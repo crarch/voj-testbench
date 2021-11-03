8c78319ba2475779ca98361e59cb4bf95d6d44c2
hilaolu <i@phyzait.moe>
2021-11-03 21:09:20 +0800

    update

--- 2001/0.v
+++ 2001/0.v
@@ -85,7 +85,7 @@ module adder_tb();
 	
     
     always@(*)begin
-        mismatch=(r_led===3'bxxx)?1'b0:(y_led!==r_led);
+        mismatch=(r_led===3'bXXX)?1'b0:(y_led!==r_led);
     end
 endmodule
 
