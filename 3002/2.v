692527e46c4256acf80ee09f538125b010ac3f0d
hilaolu <i@phyzait.moe>
2021-11-12 21:57:30 +0800

    update

--- 3002/2.v
+++ 3002/2.v
@@ -67,7 +67,10 @@ module adder_tb();
         #1;
         i_rst=0;
         for(int i=0;i<29;i++)begin
-            i_rst=($urandom%19==0);
+            i_switch=$urandom%8;
+            if($urandom%19==0)begin
+                i_rst=1;
+            end
             #1;
         end
 	$finish;
